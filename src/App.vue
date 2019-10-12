52ecdcb7a22a14cb41dca9e8f119365ec9c7a618
Toshifumi Anan <toshifumi.anan@uzabase.com>
2019-10-12 10:52:04 +0900

    add login

--- src/App.vue
+++ src/App.vue
@@ -16,19 +16,15 @@
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+      <router-view></router-view>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld,
-  },
+  components: {},
   data: () => ({
     //
   }),
