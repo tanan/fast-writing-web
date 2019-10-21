90885faa73c82465424cfbcc728e1d48cb96b112
Toshifumi Anan <toshifumi.anan@uzabase.com>
2019-10-22 08:45:38 +0900

    add pull before run

--- run.sh
+++ run.sh
@@ -2,4 +2,5 @@
 
 docker stop fast-writing-web
 docker rm fast-writing-web
+docker pull gcr.io/anan-project/github.com/tanan/fast-writing-web:master
 docker run --network fast-writing-network -d -p 80:80 --name fast-writing-web -v /opt/work/git/tanan/fast-writing/nginx/conf.d:/etc/nginx/conf.d gcr.io/anan-project/github.com/tanan/fast-writing-web:master
