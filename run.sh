080e3a3797ee1cf7bfffae793b3cf571649672b9
root <root@fast-writing01.c.anan-project.internal>
2019-10-22 09:20:45 +0000

    fix path

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 docker stop fast-writing-web
 docker rm fast-writing-web
 docker pull gcr.io/anan-project/github.com/tanan/fast-writing-web:master
-docker run --network fast-writing-network -d -p 80:80 --name fast-writing-web -v /opt/work/git/tanan/fast-writing/nginx/conf.d:/etc/nginx/conf.d gcr.io/anan-project/github.com/tanan/fast-writing-web:master
+docker run --network fast-writing-network -d -p 80:80 --name fast-writing-web -v /opt/work/git/tanan/fast-writing-web/nginx/conf.d:/etc/nginx/conf.d gcr.io/anan-project/github.com/tanan/fast-writing-web:master
